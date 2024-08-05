b788f4db877e1c5e67565174a3e405a904f3a75d
mlbahja <mlbahja28@gmail.com>
2024-08-05 19:12:28 +0100

    j

---
+++ checkpoint1/brackets/brackets.go
@@ -0,0 +1,27 @@
+package Brackets
+
+import "os"
+
+func Brackets(s string) string {
+	var str string
+	for _, v := range s {
+		if v == '[' || v == '{' || v == '(' || v == ']' || v == '}' || v == ')' {
+			str += string(v)
+		}
+	}
+	for i := 0; i < len(str); i++ {
+		if i+2 <= len(str) && (str[i:i+2] == "()" || str[i:i+2] == "{}" || str[i:i+2] == "[]") {
+			str = str[:i] + str[i+2:]
+			i = -1
+		}
+	}
+	if len(str) != 0 {
+		return "Error"
+	}
+	return "Ok"
+}
+
+func main() {
+	s := os.Args[1]
+	Brackets(s)
+}
